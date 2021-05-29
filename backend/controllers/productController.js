f22f46f2979ad04a5fbc345ec4797d172187ef4a
Doug Perez <perezcpt@gmail.com>
2021-05-29 15:57:07 -0700

    admin create product complete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,4 +24,70 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+//@desc Delete a single products
+//@route DELETE /api/products/:id
+//@access Private
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not deleted");
+  }
+});
+
+//@desc create a single products
+//@route post /api/products/
+//@access Private/admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jps",
+    brand: "Sample brand",
+    category: "sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample Description",
+  });
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+//@desc update a single products
+//@route put /api/products/:id
+//@access Private/admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
