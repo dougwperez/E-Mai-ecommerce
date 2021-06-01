4221684aed918a61aacbee12665b1ef1eb7319f6
Doug Perez <perezcpt@gmail.com>
2021-05-31 22:50:17 -0700

    revise user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,9 +2,9 @@ import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
-//@desc Auth User & get token
-//@route POST /api/users/login
-//@access Public
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -24,9 +24,9 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-//@desc Register a new user
-//@route POST /api/users
-//@access Public
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -57,9 +57,9 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-//@desc get user profile
-//@route GET /api/users/login
-//@access private
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -72,13 +72,13 @@ const getUserProfile = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404);
-    throw new Error("User Not Found");
+    throw new Error("User not found");
   }
 });
 
-//@desc Update user profile
-//@route PUT /api/users/profile
-//@access private
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -100,56 +100,57 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404);
-    throw new Error("User Not Found");
+    throw new Error("User not found");
   }
 });
 
-//@desc get all users
-//@route GET /api/users
-//@access private/Admin
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.json(users);
 });
 
-//@desc Delete a user
-//@route DELETE /api/users/:id
-//@access Private/Admin
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
+
   if (user) {
     await user.remove();
-    res.json({ message: "user removed" });
+    res.json({ message: "User removed" });
   } else {
     res.status(404);
     throw new Error("User not found");
   }
 });
 
-//@desc get user by id
-//@route GET /api/users/:id
-//@access private/Admin
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id).select("-password");
+
   if (user) {
     res.json(user);
   } else {
     res.status(404);
     throw new Error("User not found");
   }
-  res.json(users);
 });
 
-//@desc Update user
-//@route PUT /api/users/:id
-//@access private/admin
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
 
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
-    user.isAdmin = req.body.isAdmin === true ? true : false;
+    user.isAdmin = req.body.isAdmin;
 
     const updatedUser = await user.save();
 
@@ -161,7 +162,7 @@ const updateUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404);
-    throw new Error("User Not Found");
+    throw new Error("User not found");
   }
 });
 
